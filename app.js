f545b37c41fa44321e58f1af26cd943c47ac1407
Eliana <abbychamorro@gmail.com>
2025-03-16 15:39:24 -0300

    agregue funcionalidades a file app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos= [];
+
+
+function limpiarCaja(){
+    document.getElementById('amigo').value= "";
+}
+
+
+function agregarAmigo(){
+    let amigo = (document.getElementById('amigo').value);
+    console.log(amigo);
+    
+    if (amigo === ""){
+        alert("Por favor ingrese un nombre valido");
+    }
+    else {
+        listaAmigos.push(amigo);
+        limpiarCaja();
+    }
+
+}
